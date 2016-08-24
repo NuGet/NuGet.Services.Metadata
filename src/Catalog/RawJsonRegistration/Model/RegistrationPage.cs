f8e111a896a0e1c63406f567b73a64aec12a7636
Maarten Balliauw <maarten@maartenballiauw.be>
2016-08-24 13:39:14 +0200

    requireLicenseAcceptance is a boolean, not a string

--- src/Catalog/RawJsonRegistration/Model/RegistrationPage.cs
+++ src/Catalog/RawJsonRegistration/Model/RegistrationPage.cs
@@ -105,7 +105,7 @@ namespace NuGet.Services.Metadata.Catalog.RawJsonRegistration.Model
                 if (catalogEntry["summary"] == null) catalogEntry["summary"] = string.Empty;
                 if (catalogEntry["title"] == null) catalogEntry["title"] = string.Empty;
                 if (catalogEntry["tags"] == null) catalogEntry["tags"] = new JArray(string.Empty);
-                if (catalogEntry["requireLicenseAcceptance"] == null) catalogEntry["requireLicenseAcceptance"] = bool.FalseString;
+                if (catalogEntry["requireLicenseAcceptance"] == null) catalogEntry["requireLicenseAcceptance"] = false;
 
                 // Loop dependency groups and make sure they all have a registration URL property
                 var dependencyGroupsEntry = catalogEntry["dependencyGroups"] as JArray;
