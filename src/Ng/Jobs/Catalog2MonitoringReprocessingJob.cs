07dde6e1bf04aeeea908b2cd1a31d7a9d2c2d65e
Scott Bommarito <scottbom@microsoft.com>
2017-09-26 16:55:35 -0700

    should queue deletes

--- src/Ng/Jobs/Catalog2MonitoringReprocessingJob.cs
+++ src/Ng/Jobs/Catalog2MonitoringReprocessingJob.cs
@@ -54,7 +54,6 @@ namespace Ng.Jobs
             var entries = await _catalogIndexReader.GetEntries();
 
             var packageEntries = entries
-                .Where(c => !c.IsDelete())
                 .GroupBy(c => new PackageIdentity(c.Id, c.Version))
                 .Select(g => g.OrderByDescending(c => c.CommitTimeStamp).First());
 
