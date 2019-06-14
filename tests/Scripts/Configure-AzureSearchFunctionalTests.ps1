a38d1667f3d8961f85f808604b66b62d2d6a5261
Shishir H <shishirh@microsoft.com>
2019-06-14 15:38:39 -0700

    remove unnecessary flags

--- tests/Scripts/Configure-AzureSearchFunctionalTests.ps1
+++ tests/Scripts/Configure-AzureSearchFunctionalTests.ps1
@@ -1,12 +1,6 @@
 [CmdletBinding(DefaultParameterSetName='RegularBuild')]
 param (
-    [string]$ServiceRoot,
-    [string]$Slot,
-    [string]$CloudServiceName,
-    [string]$SubscriptionId,
-    [string]$ApplicationId,
-    [string]$TenantId,
-    [string]$AzureCertificateThumbprint
+    [string]$Slot
 )
 
 Write-Host "Setting environment variable for the slot: " $Slot
