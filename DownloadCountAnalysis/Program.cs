0dd9d3bf6772f828ef7ecc09bcbc7a28fb8f0274
Nikolche Kolev <nikolev@microsoft.com>
2019-06-25 17:47:48 -0700

    add more work

---
+++ DownloadCountAnalysis/Program.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.Extensions.Logging;
+using NuGet.Indexing;
+using System;
+
+namespace DownloadCountAnalysis
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var downloads = new Downloads();
+            var logger = new LoggerFactory().CreateLogger<NuGetSearcherManager>();
+
+            downloads.Load("file name", logger);
+
+        }
+    }
+}
--- src/NuGet.Indexing/Downloads.cs
+++ src/NuGet.Indexing/Downloads.cs
@@ -3,6 +3,7 @@
 
 using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using Microsoft.Extensions.Logging;
 using Newtonsoft.Json;
@@ -48,6 +49,61 @@ namespace NuGet.Indexing
             set
             {
                 _byDocId[docId] = value;
+            }
+        }
+
+        public void Load(string name, FrameworkLogger logger)
+        {
+            // The data in downloads.v1.json will be an array of Package records - which has Id, Array of Versions and download count.
+            // Sample.json : [["AutofacContrib.NSubstitute",["2.4.3.700",406],["2.5.0",137]],["Assman.Core",["2.0.7",138]]....
+            using (var jsonReader = new JsonTextReader(new StringReader(name)))
+            {
+                try
+                {
+                    jsonReader.Read();
+
+                    while (jsonReader.Read())
+                    {
+                        try
+                        {
+                            if (jsonReader.TokenType == JsonToken.StartArray)
+                            {
+                                JToken record = JToken.ReadFrom(jsonReader);
+                                string id = String.Intern(record[0].ToString().ToLowerInvariant());
+
+                                // The second entry in each record should be an array of versions, if not move on to next entry.
+                                // This is a check to safe guard against invalid entries.
+                                if (record.Count() == 2 && record[1].Type != JTokenType.Array)
+                                {
+                                    continue;
+                                }
+
+                                if (!_downloads.ContainsKey(id))
+                                {
+                                    _downloads.Add(id, new DownloadsByVersion());
+                                }
+                                var versions = _downloads[id];
+
+                                foreach (JToken token in record)
+                                {
+                                    if (token != null && token.Count() == 2)
+                                    {
+                                        string version = String.Intern(token[0].ToString().ToLowerInvariant());
+                                        versions[version] = token[1].ToObject<int>();
+                                    }
+                                }
+                            }
+                        }
+                        catch (JsonReaderException ex)
+                        {
+                            logger.LogInformation("Invalid entry found in downloads.v1.json. Exception Message : {0}", ex.Message);
+                        }
+                    }
+                }
+                catch (JsonReaderException ex)
+                {
+                    logger.LogError("Data present in downloads.v1.json is invalid. Couldn't get download data.", ex);
+                }
             }
         }
 
