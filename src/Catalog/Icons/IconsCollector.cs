c91e25379ed13a6c09c52ca48709dad42c0bea4d
Andrei Grigorev <angrigor@microsoft.com>
2019-08-14 17:24:27 -0700

    Job skeleton

---
+++ src/Catalog/Icons/IconsCollector.cs
@@ -0,0 +1,43 @@
+﻿// Copyright (c) .NET Foundation. All rights reserved.
+// Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Threading;
+using System.Threading.Tasks;
+using Newtonsoft.Json.Linq;
+
+namespace NuGet.Services.Metadata.Catalog.Icons
+{
+    public class IconsCollector : CommitCollector
+    {
+        public IconsCollector(
+            Uri index,
+            ITelemetryService telemetryService,
+            Func<HttpMessageHandler> httpHandlerFactory)
+            : base(index, telemetryService, httpHandlerFactory)
+        {
+        }
+
+        protected override Task<IEnumerable<CatalogCommitItemBatch>> CreateBatchesAsync(
+            IEnumerable<CatalogCommitItem> catalogItems)
+        {
+            var maxCommitTimestamp = catalogItems.Max(x => x.CommitTimeStamp);
+
+            return Task.FromResult<IEnumerable<CatalogCommitItemBatch>>(new[]
+            {
+                new CatalogCommitItemBatch(
+                    catalogItems,
+                    key: null,
+                    commitTimestamp: maxCommitTimestamp),
+            });
+        }
+
+        protected override Task<bool> OnProcessBatchAsync(CollectorHttpClient client, IEnumerable<CatalogCommitItem> items, JToken context, DateTime commitTimeStamp, bool isLastBatch, CancellationToken cancellationToken)
+        {
+            return Task.FromResult(true);
+        }
+    }
+}
---
+++ src/Ng/Jobs/Catalog2IconJob.cs
@@ -0,0 +1,53 @@
+﻿// Copyright (c) .NET Foundation. All rights reserved.
+// Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
+
+using System;
+using System.Collections.Generic;
+using System.Threading;
+using System.Threading.Tasks;
+using Microsoft.Extensions.Logging;
+using NuGet.Services.Configuration;
+using NuGet.Services.Metadata.Catalog;
+using NuGet.Services.Metadata.Catalog.Icons;
+using NuGet.Services.Metadata.Catalog.Persistence;
+
+namespace Ng.Jobs
+{
+    public class Catalog2IconJob : LoopingNgJob
+    {
+        private IconsCollector _collector;
+        private DurableCursor _front;
+
+        public Catalog2IconJob(ITelemetryService telemetryService, ILoggerFactory loggerFactory)
+            : base(telemetryService, loggerFactory)
+        {
+        }
+
+        protected override void Init(IDictionary<string, string> arguments, CancellationToken cancellationToken)
+        {
+            var verbose = arguments.GetOrDefault(Arguments.Verbose, false);
+            var cursorStorageFactory = CreateIconCursorStorageFactory(arguments, verbose);
+            var source = arguments.GetOrThrow<string>(Arguments.Source);
+            _collector = new IconsCollector(
+                new Uri(source),
+                TelemetryService,
+                CommandHelpers.GetHttpMessageHandlerFactory(TelemetryService, verbose));
+            var cursorStorage = cursorStorageFactory.Create();
+            _front = new DurableCursor(cursorStorage.ResolveUri("c2icursor.json"), cursorStorage, DateTime.MinValue.ToUniversalTime());
+        }
+
+        protected override async Task RunInternalAsync(CancellationToken cancellationToken)
+        {
+            bool run;
+            do
+            {
+                run = await _collector.RunAsync(_front, MemoryCursor.CreateMax(), cancellationToken);
+            } while (run);
+        }
+
+        private IStorageFactory CreateIconCursorStorageFactory(IDictionary<string, string> arguments, bool verbose)
+        {
+            return CommandHelpers.CreateSuffixedStorageFactory("Icon", arguments, verbose);
+        }
+    }
+}
