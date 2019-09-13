a89f39914e5f04a5660853cbe959d643ebfb8f23
Andrei Grigorev <angrigor@microsoft.com>
2019-09-13 14:48:35 -0700

    Custom JsonConverter to be able to read certain funny catalog leaves.

--- src/NuGet.Protocol.Catalog/Models/PackageDetailsCatalogLeaf.cs
+++ src/NuGet.Protocol.Catalog/Models/PackageDetailsCatalogLeaf.cs
@@ -4,6 +4,7 @@
 using System;
 using System.Collections.Generic;
 using Newtonsoft.Json;
+using NuGet.Protocol.Catalog.Serialization;
 
 namespace NuGet.Protocol.Catalog
 {
@@ -62,6 +63,7 @@ namespace NuGet.Protocol.Catalog
         public string ProjectUrl { get; set; }
 
         [JsonProperty("releaseNotes")]
+        [JsonConverter(typeof(BareStringOrArrayConcatenatingConverter))]
         public string ReleaseNotes { get; set; }
 
         [JsonProperty("requireLicenseAgreement")]
---
+++ tests/NuGet.Protocol.Catalog.Tests/BareStringOrArrayConcatenatingConverterFacts.cs
@@ -0,0 +1,47 @@
+﻿// Copyright (c) .NET Foundation. All rights reserved.
+// Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+using Xunit;
+
+namespace NuGet.Protocol.Catalog
+{
+    public class BareStringOrArrayConcatenatingConverterFacts
+    {
+        private class SerializationTestObject
+        {
+            [JsonProperty]
+            [JsonConverter(typeof(BareStringOrArrayConcatenatingConverter))]
+            public string StringProperty { get; set; }
+        }
+
+        [Fact]
+        public void ReadsBareStrings()
+        {
+            const string input = "{\"stringProperty\": \"foo\"}";
+            var obj = JsonConvert.DeserializeObject<SerializationTestObject>(input);
+            Assert.Equal("foo", obj.StringProperty);
+        }
+
+        [Fact]
+        public void ReadsStringArrays()
+        {
+            const string input = "{\"stringProperty\": [\"foo\", \"bar\"]}";
+            var obj = JsonConvert.DeserializeObject<SerializationTestObject>(input);
+            Assert.Equal("foobar", obj.StringProperty);
+        }
+
+        [Fact]
+        public void WritesStrings()
+        {
+            var obj = new SerializationTestObject { StringProperty = "foo" };
+            var str = JsonConvert.SerializeObject(obj, Formatting.None);
+            Assert.Equal("{\"StringProperty\":\"foo\"}", str);
+        }
+    }
+}
