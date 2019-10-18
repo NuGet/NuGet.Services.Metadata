cb2f5b87e6ee25d763ff1dd01f4bc8ac9cb64751
Peter Yu <ryuyu@microsoft.com>
2019-10-18 13:19:52 -0700

    Correct usage of telemetry.context.properties.

--- tests/NgTests/JobPropertiesTelemetryInitializerTests.cs
+++ tests/NgTests/JobPropertiesTelemetryInitializerTests.cs
@@ -58,9 +58,11 @@ namespace NgTests
             _telemetryService.SetupGet(x => x.GlobalDimensions)
                 .Returns(_globalDimensions);
 
+            var telemetry = new TestableTelemetry();
+
             var initializer = new JobPropertiesTelemetryInitializer(_telemetryService.Object);
 
-            initializer.Initialize(_telemetry.Object);
+            initializer.Initialize(telemetry);
 
             Assert.Empty(_telemetryContext.GlobalProperties);
         }
@@ -73,23 +75,19 @@ namespace NgTests
                 { "a", "b" },
                 { "c", "d" }
             };
-            var telemetryContext = new TelemetryContext();
-            var telemetry = new Mock<ITelemetry>();
+            var telemetry = new TestableTelemetry();
             var telemetryService = new Mock<ITelemetryService>();
-
-            telemetry.SetupGet(x => x.Context)
-                .Returns(telemetryContext);
 
             telemetryService.SetupGet(x => x.GlobalDimensions)
                 .Returns(globalDimensions);
 
             var initializer = new JobPropertiesTelemetryInitializer(telemetryService.Object);
 
-            initializer.Initialize(telemetry.Object);
+            initializer.Initialize(telemetry);
 
-            Assert.Equal(2, telemetryContext.GlobalProperties.Count);
-            Assert.Equal("b", telemetryContext.GlobalProperties["a"]);
-            Assert.Equal("d", telemetryContext.GlobalProperties["c"]);
+            Assert.Equal(2, telemetry.Properties.Count);
+            Assert.Equal("b", telemetry.Properties["a"]);
+            Assert.Equal("d", telemetry.Properties["c"]);
         }
     }
 }
